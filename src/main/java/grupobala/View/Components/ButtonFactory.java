eb979e3d94af08837a4502ad7f7e04c25e9cb162
Antônio Hugo <hugorplobo@gmail.com>
2022-10-09 21:41:38 -0300

    feat: add button factory

---
+++ src/main/java/grupobala/View/Components/ButtonFactory.java
@@ -0,0 +1,14 @@
+package grupobala.View.Components;
+
+import grupobala.View.Components.ComponentFactory.ComponentFactory;
+import javafx.scene.control.Button;
+
+public class ButtonFactory implements ComponentFactory {
+    public Button getComponent() {
+        Button button = new Button();
+        button.getStyleClass().add("financi-button");
+        button.getStylesheets().add("file:src/main/java/grupobala/View/Components/ButtonFactory.css");
+
+        return button;
+    }
+}
