341b935de156777d524d5e5ceaded78ca3785e08
Felipe Rodrigues de Santana Freitag <felipefreitag08@hotmail.com>
2022-10-10 19:50:40 -0300

    test: update TestDBController to be independent

--- src/test/java/grupobala/Database/TestDBController.java
+++ src/test/java/grupobala/Database/TestDBController.java
@@ -2,21 +2,18 @@ package grupobala.Database;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
-import java.sql.SQLException;
-
-import org.junit.jupiter.api.Order;
-import org.junit.jupiter.api.Test;
-
 import grupobala.Database.DBController.DBController;
+import java.sql.*;
+import java.sql.SQLException;
+import org.junit.jupiter.api.Test;
 
 public class TestDBController {
 
     @Test
-    @Order(1)
     public void testRegistration() throws SQLException {
         DBController db = new DBController();
 
-        db.dropEverything();
+        TestDBController.truncateTablesForTest();
 
         boolean result = db.signUp("financi", "1234", "Financi", 0);
 
@@ -24,21 +21,37 @@ public class TestDBController {
     }
 
     @Test
-    public void testLogin() throws Exception {
+    public void testLogin() throws SQLException {
         DBController db = new DBController();
-        db.dropEverything();
+
+        TestDBController.truncateTablesForTest();
+
         db.signUp("testLogin", "1234", "Login", 0);
+
         boolean result = db.login("testLogin", "1234");
 
         assertTrue(result);
     }
 
     @Test
-    public void testRegistrationShouldFail() throws Exception {
+    public void testRegistrationShouldFail() throws SQLException {
         DBController db = new DBController();
-        db.dropEverything();
-        db.signUp("testRegistrationShouldFail", "1234", "testRegistrationShouldFail", 0);
-        boolean result = db.signUp("testRegistrationShouldFail", "1234", "Financi", 0);
+
+        TestDBController.truncateTablesForTest();
+
+        db.signUp(
+            "testRegistrationShouldFail",
+            "1234",
+            "testRegistrationShouldFail",
+            0
+        );
+
+        boolean result = db.signUp(
+            "testRegistrationShouldFail",
+            "1234",
+            "Financi",
+            0
+        );
 
         assertTrue(!result);
     }
@@ -46,9 +59,32 @@ public class TestDBController {
     @Test
     public void testLoginShouldFail() throws SQLException {
         DBController db = new DBController();
-        db.dropEverything();
+
+        TestDBController.truncateTablesForTest();
+
         boolean result = db.login("usuarioNaoCadastrado", "1234");
 
         assertTrue(!result);
+    }
+
+    private static void truncateTablesForTest() throws SQLException {
+        Statement statement = DriverManager
+            .getConnection(
+                "jdbc:postgresql://localhost:5432/financi?user=postgres&password=postgres"
+            )
+            .createStatement();
+
+        String[] queries = {
+            "TRUNCATE TABLE usuario CASCADE",
+            "TRUNCATE TABLE meta CASCADE",
+            "TRUNCATE TABLE aulaassistida CASCADE",
+            "TRUNCATE TABLE aula CASCADE",
+            "TRUNCATE TABLE movimentacao CASCADE",
+            "TRUNCATE TABLE categoria CASCADE",
+        };
+
+        for (String query : queries) {
+            statement.executeUpdate(query);
+        }
     }
 }
