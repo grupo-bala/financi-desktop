cfe283794d458aa7ccdfdab5687365f1b1d8a13f
Felipe Rodrigues de Santana Freitag <felipefreitag08@hotmail.com>
2022-10-12 16:35:20 -0300

    feat: add set methods in ITransaction and implement in Transaction

--- src/main/java/grupobala/Entities/Transaction/ITransaction/ITransaction.java
+++ src/main/java/grupobala/Entities/Transaction/ITransaction/ITransaction.java
@@ -6,11 +6,19 @@ import java.util.Date;
 public interface ITransaction {
     public int getId();
 
-    public double getValor();
+    public double getValue();
 
     public String getTitle();
 
     public CategoryEnum getCategory();
 
     public Date getDate();
+
+    public void setValue(double value);
+
+    public void setTitle(String title);
+
+    public void setCategory(CategoryEnum category);
+
+    public void setDate(Date date);
 }
