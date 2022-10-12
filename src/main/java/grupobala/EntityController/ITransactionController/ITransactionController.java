c1e62acf9901e034fde6eb006e50bea510cab829
Automated Publisher <actions@users.noreply.github.com>
2022-10-12 21:00:51 +0000

    Automated publish: Wed Oct 12 21:00:51 UTC 2022 54644ce90f94f90d0b59cdbd8dd63abc25bdab2f

--- src/main/java/grupobala/EntityController/ITransactionController/ITransactionController.java
+++ src/main/java/grupobala/EntityController/ITransactionController/ITransactionController.java
@@ -1,17 +1,22 @@
 package grupobala.EntityController.ITransactionController;
 
+import grupobala.Entities.Category.CategoryEnum;
+import grupobala.Entities.Transaction.ITransaction.ITransaction;
 import java.sql.Date;
 import java.sql.SQLException;
 
-import grupobala.Entities.Category.CategoryEnum;
-import grupobala.Entities.Transaction.ITransaction.ITransaction;
-
 public interface ITransactionController {
-    
-    public ITransaction addTransaction(String username, double value, String title, CategoryEnum category, Date date) throws SQLException;
+    public ITransaction addTransaction(
+        String username,
+        double value,
+        String title,
+        CategoryEnum category,
+        Date date
+    ) throws SQLException;
 
-    public void updateTransaction(String username, ITransaction transaction) throws SQLException;
+    public void updateTransaction(String username, ITransaction transaction)
+        throws SQLException;
 
-    public void removeTransaction(String username, int transactionID) throws SQLException;
-
+    public void removeTransaction(String username, int transactionID)
+        throws SQLException;
 }
--- src/main/java/grupobala/EntityController/TransactionController.java
+++ src/main/java/grupobala/EntityController/TransactionController.java
@@ -1,20 +1,26 @@
 package grupobala.EntityController;
 
-import java.sql.Date;
-import java.sql.SQLException;
-
 import grupobala.Entities.Category.CategoryEnum;
 import grupobala.Entities.Transaction.ITransaction.ITransaction;
 import grupobala.EntityController.ITransactionController.ITransactionController;
+import java.sql.Date;
+import java.sql.SQLException;
 
-public class TransactionController implements ITransactionController{
-    
-    public ITransaction addTransaction(String username, double value, String title, CategoryEnum category, Date date) throws SQLException {
+public class TransactionController implements ITransactionController {
+
+    public ITransaction addTransaction(
+        String username,
+        double value,
+        String title,
+        CategoryEnum category,
+        Date date
+    ) throws SQLException {
         return null;
     }
 
-    public void updateTransaction(String username, ITransaction transaction) throws SQLException {}
+    public void updateTransaction(String username, ITransaction transaction)
+        throws SQLException {}
 
-    public void removeTransaction(String username, int transactionID) throws SQLException {}
-
+    public void removeTransaction(String username, int transactionID)
+        throws SQLException {}
 }
