571369064aafb2371f1f15a3abd00fe5454c076a
Felipe Rodrigues de Santana Freitag <felipefreitag08@hotmail.com>
2022-10-12 13:58:04 -0300

    feat: update the code and sql in setup create tables

--- src/main/java/grupobala/Database/Setup/Setup.java
+++ src/main/java/grupobala/Database/Setup/Setup.java
@@ -40,12 +40,13 @@ public class Setup {
 
     private static void createTables(Statement statement) throws SQLException {
         String[] queries = {
-            "CREATE TABLE usuario (id SERIAL PRIMARY KEY, nome TEXT NOT NULL, nomeUsuario TEXT UNIQUE NOT NULL, senha TEXT NOT NULL, rendaFixa FLOAT NOT NULL)",
-            "CREATE TABLE categoria (id SERIAL PRIMARY KEY, nome TEXT NOT NULL)",
-            "CREATE TABLE meta (id SERIAL PRIMARY KEY, titulo TEXT NOT NULL, idUsuario INTEGER NOT NULL, valorMeta FLOAT NOT NULL, valorAtual FLOAT NOT NULL, dataLimite DATE, FOREIGN KEY (idUsuario) REFERENCES usuario(id))",
-            "CREATE TABLE aula (id SERIAL PRIMARY KEY , titulo TEXT NOT NULL, descricao TEXT NOT NULL, videoURL TEXT NOT NULL, duracaoSegundos INTEGER NOT NULL, thumbnailURL TEXT NOT NULL)",
-            "CREATE TABLE aulaAssistida (id SERIAL PRIMARY KEY, idUsuario INTEGER NOT NULL, idAula INTEGER NOT NULL, UNIQUE(idUsuario, idAula), FOREIGN KEY (idUsuario) REFERENCES usuario(id), FOREIGN KEY (idAula) REFERENCES aula(id))",
-            "CREATE TABLE movimentacao (id SERIAL PRIMARY KEY, idUsuario INTEGER NOT NULL, valor FLOAT NOT NULL, data DATE NOT NULL, idCategoria INTEGER NOT NULL, titulo TEXT NOT NULL, descricao TEXT NOT NULL, entrada BOOLEAN NOT NULL, FOREIGN KEY (idUsuario) REFERENCES usuario(id), FOREIGN KEY (idCategoria) REFERENCES categoria(id))",
+            "CREATE TABLE usuario (id SERIAL PRIMARY KEY, nome TEXT NOT NULL, nomeusuario TEXT NOT NULL UNIQUE, senha TEXT NOT NULL, rendafixa FLOAT NOT NULL)",
+            "CREATE TABLE categoria (id SERIAL PRIMARY KEY, nome TEXT NOT NULL UNIQUE)",
+            "CREATE TABLE meta (id SERIAL PRIMARY KEY, titulo TEXT NOT NULL UNIQUE, idusuario INTEGER NOT NULL, valormeta FLOAT NOT NULL, valoratual FLOAT NOT NULL, datalimite DATE, FOREIGN KEY (idusuario) REFERENCES usuario(id))",
+            "CREATE TABLE aula (id SERIAL PRIMARY KEY , titulo TEXT NOT NULL UNIQUE, descricao TEXT NOT NULL, videourl TEXT NOT NULL, duracaosegundos INTEGER NOT NULL, thumbnailurl TEXT NOT NULL)",
+            "CREATE TABLE aulaassistida (id SERIAL PRIMARY KEY, idusuario INTEGER NOT NULL, idaula INTEGER NOT NULL, UNIQUE(idusuario, idaula), FOREIGN KEY (idusuario) REFERENCES usuario(id), FOREIGN KEY (idaula) REFERENCES aula(id))",
+            "CREATE TABLE movimentacao (id SERIAL PRIMARY KEY, idusuario INTEGER NOT NULL, valor FLOAT NOT NULL, data DATE NOT NULL, idcategoria INTEGER NOT NULL, titulo TEXT NOT NULL, entrada BOOLEAN NOT NULL, FOREIGN KEY (idusuario) REFERENCES usuario(id), FOREIGN KEY (idcategoria) REFERENCES categoria(id))",
+            "INSERT INTO categoria(nome) VALUES ('comida'), ('roupa'), ('saúde'), ('entretenimento'), ('pagamentos'), ('outros')"
         };
 
         for (String query : queries) {
