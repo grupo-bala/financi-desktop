48a865db1f5da0670969fd6bc9fda0665f1238ad
Antônio Hugo <hugorplobo@gmail.com>
2022-10-13 19:44:07 -0300

    feat: add popup component

---
+++ src/main/java/grupobala/View/Components/Popup/PopupComponent.java
@@ -0,0 +1,38 @@
+package grupobala.View.Components.Popup;
+
+
+import grupobala.View.Components.Component.Component;
+import javafx.scene.layout.Region;
+import javafx.scene.layout.StackPane;
+
+public class PopupComponent implements Component {
+
+    private StackPane container = new StackPane();
+    private Region background = new Region();
+
+    public PopupComponent() {
+        this.hidePopup();
+        background.getStyleClass().add("financi-popup-rect");
+        container.getStyleClass().addAll("financi-popup-container");
+        container.getStylesheets().add(
+            "file:src/main/java/grupobala/View/Components/Popup/Popup.css"
+        );
+
+        container.getChildren().add(background);
+        background.setOnMouseClicked(e -> {
+            this.hidePopup();
+        });
+    }
+
+    public StackPane getComponent() {
+        return container;
+    }
+
+    public void showPopup() {
+        container.setVisible(true);
+    }
+
+    public void hidePopup() {
+        container.setVisible(false);
+    }
+}
