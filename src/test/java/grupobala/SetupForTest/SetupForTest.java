adaff261c409fb1c4306bd02801a57a317f36a91
Felipe Rodrigues de Santana Freitag <felipefreitag08@hotmail.com>
2022-10-12 23:16:31 -0300

    refactor: create a setup class that other tests can use to set the ambient

---
+++ src/test/java/grupobala/SetupForTest/SetupForTest.java
@@ -0,0 +1,57 @@
+package grupobala.SetupForTest;
+
+import java.sql.*;
+
+public class SetupForTest {
+    public static void truncateTables() throws SQLException {
+        Connection connection = DriverManager.getConnection(
+            "jdbc:postgresql://localhost:5432/financi?user=postgres&password=postgres"
+        );
+
+        Statement statement = connection.createStatement();
+
+        String[] queries = {
+            "TRUNCATE TABLE usuario CASCADE",
+            "TRUNCATE TABLE meta CASCADE",
+            "TRUNCATE TABLE aulaassistida CASCADE",
+            "TRUNCATE TABLE aula CASCADE",
+            "TRUNCATE TABLE movimentacao CASCADE",
+        };
+
+        for (String query : queries) {
+            statement.executeUpdate(query);
+        }
+
+        connection.close();
+    }
+
+    public static void removeFinanciDB() throws SQLException {
+        try {
+            Connection rootConnection = DriverManager.getConnection(
+                "jdbc:postgresql://localhost:5432/?user=postgres&password=postgres"
+            );
+
+            String query = "DROP DATABASE financi";
+
+            rootConnection.createStatement().executeUpdate(query);
+
+            rootConnection.close();
+        } catch (Exception e) {
+            System.out.println("Financi database already dropped");
+        }
+    }
+
+    public static void addFinanciUser() throws SQLException {
+        Connection connection = DriverManager.getConnection(
+            "jdbc:postgresql://localhost:5432/financi?user=postgres&password=postgres"
+        );
+
+        Statement statement = connection.createStatement();
+
+        String query = "INSERT INTO usuario(nome, nomeusuario, senha, rendafixa) VALUES ('Financi', 'financi', '1234', 1000)";
+
+        statement.executeUpdate(query);
+
+        connection.close();
+    }
+}
