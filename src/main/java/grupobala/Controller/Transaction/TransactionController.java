66d194e4d802ec57c31eb5f311c93b8dd235e34c
João Paulo Freitas Queiroz <Joaop3595@gmail.com>
2022-10-15 01:12:43 -0300

    feat: implement remove transaction method in TransactionController

--- src/main/java/grupobala/Controller/Transaction/TransactionController.java
+++ src/main/java/grupobala/Controller/Transaction/TransactionController.java
@@ -1,26 +1,42 @@
 package grupobala.Controller.Transaction;
 
+import grupobala.Database.Connection.DBConnection;
+import grupobala.Database.Transaction.DBTransaction;
 import grupobala.Controller.Transaction.ITransactionController.ITransactionController;
 import grupobala.Entities.Category.CategoryEnum;
 import grupobala.Entities.Transaction.ITransaction.ITransaction;
 import java.sql.Date;
 import java.sql.SQLException;
 
+
 public class TransactionController implements ITransactionController {
 
+    @Override
     public ITransaction addTransaction(
         String username,
-        double value,
+        double wage,
         String title,
         CategoryEnum category,
         Date date
-    ) throws SQLException {
+    ) throws Exception {
         return null;
     }
 
+    @Override
     public void updateTransaction(String username, ITransaction transaction)
-        throws SQLException {}
+        throws Exception {}
 
+    @Override
     public void removeTransaction(String username, int transactionID)
-        throws SQLException {}
+        throws Exception {
+            DBTransaction dbTransaction = new DBTransaction(new DBConnection());
+
+            try { 
+                dbTransaction.removeTransaction(username, transactionID);
+            }
+            catch(SQLException error){
+                throw new Exception("Não foi possível apagar a transação");
+            }
+        
+        }
 }
