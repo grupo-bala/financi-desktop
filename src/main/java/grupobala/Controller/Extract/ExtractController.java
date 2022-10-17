276ba6d3121c575109885eb9f70d3c918efec633
Automated Publisher <actions@users.noreply.github.com>
2022-10-17 04:25:52 +0000

    Automated publish: Mon Oct 17 04:25:52 UTC 2022 3a80a37552ecc7a3f85b5ef6fe27405a6093cc71

--- src/main/java/grupobala/Controller/Extract/ExtractController.java
+++ src/main/java/grupobala/Controller/Extract/ExtractController.java
@@ -1,19 +1,18 @@
 package grupobala.Controller.Extract;
 
-import java.util.Date;
-import java.sql.SQLException;
-import java.text.ParseException;
-import java.util.ArrayList;
-
 import grupobala.Controller.Extract.IExtractController.IExtractController;
 import grupobala.Database.Connection.DBConnection;
 import grupobala.Database.Extract.DBExtract;
 import grupobala.Entities.Extract.Extract;
 import grupobala.Entities.Extract.IExtract.IExtract;
 import grupobala.Entities.Transaction.ITransaction.ITransaction;
+import java.sql.SQLException;
+import java.text.ParseException;
+import java.util.ArrayList;
+import java.util.Date;
 
-public class ExtractController implements IExtractController{
- 
+public class ExtractController implements IExtractController {
+
     DBExtract dbExtract;
 
     public ExtractController() {
@@ -21,11 +20,15 @@ public class ExtractController implements IExtractController{
     }
 
     @Override
-	public IExtract getExtract(int id, Date initial, Date end) throws SQLException, ParseException{
-        
-        ArrayList<ITransaction> transactions = dbExtract.getExtract(id, initial, end);
-		IExtract extract = new Extract(transactions);
+    public IExtract getExtract(int id, Date initial, Date end)
+        throws SQLException, ParseException {
+        ArrayList<ITransaction> transactions = dbExtract.getExtract(
+            id,
+            initial,
+            end
+        );
+        IExtract extract = new Extract(transactions);
 
         return extract;
-	}
+    }
 }
--- src/main/java/grupobala/Controller/Extract/IExtractController/IExtractController.java
+++ src/main/java/grupobala/Controller/Extract/IExtractController/IExtractController.java
@@ -1,12 +1,11 @@
 package grupobala.Controller.Extract.IExtractController;
 
+import grupobala.Entities.Extract.IExtract.IExtract;
 import java.sql.SQLException;
 import java.text.ParseException;
 import java.util.Date;
 
-import grupobala.Entities.Extract.IExtract.IExtract;
-
 public interface IExtractController {
-    
-    public IExtract getExtract(int id, Date initial, Date end) throws SQLException, ParseException;
+    public IExtract getExtract(int id, Date initial, Date end)
+        throws SQLException, ParseException;
 }
