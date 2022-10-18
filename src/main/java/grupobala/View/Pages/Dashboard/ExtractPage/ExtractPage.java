ecff2fc767ff432d53561931d2b0a53e07203b97
Automated Publisher <actions@users.noreply.github.com>
2022-10-18 03:05:44 +0000

    Automated publish: Tue Oct 18 03:05:44 UTC 2022 d9603ca379b75c4e77a7ddb35a5e3b691c73d795

--- src/main/java/grupobala/View/Pages/Dashboard/ExtractPage/ExtractPage.java
+++ src/main/java/grupobala/View/Pages/Dashboard/ExtractPage/ExtractPage.java
@@ -1,5 +1,12 @@
 package grupobala.View.Pages.Dashboard.ExtractPage;
 
+import grupobala.Entities.Category.CategoryEnum;
+import grupobala.Entities.Extract.Extract;
+import grupobala.Entities.Extract.IExtract.IExtract;
+import grupobala.Entities.Transaction.ITransaction.ITransaction;
+import grupobala.Entities.Transaction.Transaction;
+import grupobala.SetupForTest.SetupForTest;
+import grupobala.View.Pages.Page.Page;
 import java.sql.Date;
 import java.sql.SQLException;
 import java.text.DateFormat;
@@ -7,32 +14,22 @@ import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Locale;
-
-
-import grupobala.SetupForTest.SetupForTest;
-import grupobala.Entities.Category.CategoryEnum;
-import grupobala.Entities.Extract.Extract;
-import grupobala.Entities.Extract.IExtract.IExtract;
-import grupobala.Entities.Transaction.Transaction;
-import grupobala.Entities.Transaction.ITransaction.ITransaction;
-import grupobala.View.Pages.Page.Page;
 import javafx.geometry.Pos;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
 import javafx.scene.text.Text;
 
-public class ExtractPage implements Page{
-    
+public class ExtractPage implements Page {
+
     private StackPane mainPane = new StackPane();
-    
+
     private Locale localeBR;
     private DateFormat dateFormat;
     private VBox mainContainer;
 
     @Override
     public StackPane getMainPane() {
-
         localeBR = new Locale("pt", "BR");
         dateFormat = new SimpleDateFormat("dd 'de' MMM yyyy", localeBR);
 
@@ -42,26 +39,46 @@ public class ExtractPage implements Page{
         mainPane.getStyleClass().add("extract");
         mainContainer.getStyleClass().add("container");
 
-        mainPane.getStylesheets().add("file:src/main/java/grupobala/View/Pages/Dashboard/ExtractPage/ExtractPage.css");
+        mainPane
+            .getStylesheets()
+            .add(
+                "file:src/main/java/grupobala/View/Pages/Dashboard/ExtractPage/ExtractPage.css"
+            );
 
         mainPane.getChildren().add(mainContainer);
         mainContainer.getChildren().add(title);
 
         try {
             loadExtract();
-        } catch (Exception e) {
-        }
+        } catch (Exception e) {}
 
         return mainPane;
     }
 
-    
     private void loadExtract() throws SQLException, ParseException {
         //SetupForTest.truncateTables();
 
-        ITransaction transEx = new Transaction(12, 1000, "teste", CategoryEnum.CLOTHING, new Date(2020, 1, 1));
-        ITransaction transExT = new Transaction(12, -1000, "teste", CategoryEnum.CLOTHING, new Date(2020, 2, 2));
-        ITransaction transExTT = new Transaction(12, 1000, "teste", CategoryEnum.CLOTHING, new Date(2020, 1, 1));
+        ITransaction transEx = new Transaction(
+            12,
+            1000,
+            "teste",
+            CategoryEnum.CLOTHING,
+            new Date(2020, 1, 1)
+        );
+        ITransaction transExT = new Transaction(
+            12,
+            -1000,
+            "teste",
+            CategoryEnum.CLOTHING,
+            new Date(2020, 2, 2)
+        );
+        ITransaction transExTT = new Transaction(
+            12,
+            1000,
+            "teste",
+            CategoryEnum.CLOTHING,
+            new Date(2020, 1, 1)
+        );
 
         ArrayList<ITransaction> lista = new ArrayList<>();
         lista.add(transEx);
@@ -74,7 +91,6 @@ public class ExtractPage implements Page{
     }
 
     private VBox getTitlePage() {
-
         VBox container = new VBox();
         Text title = new Text("Histórico de Transações");
         title.getStyleClass().add("extract-title");
@@ -86,10 +102,9 @@ public class ExtractPage implements Page{
     }
 
     private VBox getTransactionsPreview(IExtract extract) {
-
         VBox outputs = new VBox();
         for (ITransaction t : extract) {
-            VBox tview = compilingTransactionPreview(t); 
+            VBox tview = compilingTransactionPreview(t);
 
             outputs.getChildren().add(tview);
         }
@@ -98,19 +113,23 @@ public class ExtractPage implements Page{
     }
 
     private VBox compilingTransactionPreview(ITransaction t) {
-        
         boolean isNegative = t.getValue() < 0;
 
         Text title = new Text(t.getTitle());
         Text date = new Text(dateFormat.format(t.getDate()));
-        Text value = new Text((isNegative ? "-" : "") + String.format("R$ %.2f", Math.abs(t.getValue())));
+        Text value = new Text(
+            (isNegative ? "-" : "") +
+            String.format("R$ %.2f", Math.abs(t.getValue()))
+        );
 
         VBox left = new VBox(title, date);
-        VBox right = new VBox(value);        
+        VBox right = new VBox(value);
         HBox tbox = new HBox(left, right);
         VBox tboxCont = new VBox(tbox);
 
-        value.getStyleClass().add(isNegative ? "extract-value-red" : "extract-value-green");
+        value
+            .getStyleClass()
+            .add(isNegative ? "extract-value-red" : "extract-value-green");
         tboxCont.getStyleClass().add("extract-transaction-container");
         date.getStyleClass().add("extract-transaction-date");
         tbox.getStyleClass().add("extract-transaction");
@@ -119,7 +138,4 @@ public class ExtractPage implements Page{
 
         return tboxCont;
     }
-
-
-    
 }
