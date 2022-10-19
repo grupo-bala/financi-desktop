23d71dabefc96e9ad5356042bd2e027993df2487
Automated Publisher <actions@users.noreply.github.com>
2022-10-19 16:43:05 +0000

    Automated publish: Wed Oct 19 16:43:05 UTC 2022 6cf8f4164b66360c74af9bb3a873565b35d298f8

--- src/test/java/grupobala/Controller/TestAddTransactionController.java
+++ src/test/java/grupobala/Controller/TestAddTransactionController.java
@@ -1,21 +1,21 @@
 package grupobala.Controller;
 
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
 import grupobala.Controller.Authentication.AuthenticationController;
-import grupobala.Controller.Transaction.TransactionController;
 import grupobala.Controller.Transaction.ITransactionController.ITransactionController;
+import grupobala.Controller.Transaction.TransactionController;
 import grupobala.Entities.Category.CategoryEnum;
 import grupobala.Entities.Transaction.ITransaction.ITransaction;
 import grupobala.Entities.User.User;
 import grupobala.SetupForTest.SetupForTest;
-
-import static org.junit.jupiter.api.Assertions.assertThrows;
-
 import java.util.Calendar;
 import java.util.Date;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class TestAddTransactionController {
+
     ITransactionController transactionController = new TransactionController();
     AuthenticationController authenticationController = new AuthenticationController();
     Calendar calendar = Calendar.getInstance();
@@ -30,7 +30,13 @@ public class TestAddTransactionController {
         authenticationController.signUp("financi12", "123", "financi", 100);
         authenticationController.signIn("financi12", "123");
 
-        transactionController.addTransaction(new User().getID(), wageTransaction, titleTranction, categoryTransaction, dateTransaction);
+        transactionController.addTransaction(
+            new User().getID(),
+            wageTransaction,
+            titleTranction,
+            categoryTransaction,
+            dateTransaction
+        );
 
         new User().close();
     }
@@ -38,9 +44,18 @@ public class TestAddTransactionController {
     @Test
     public void testeAddTransactionFailUserIDNoneExistent() throws Exception {
         SetupForTest.truncateTables();
-        
-        assertThrows(Exception.class, ()->{
-            transactionController.addTransaction(-1, wageTransaction, titleTranction, categoryTransaction, dateTransaction);
-        });
+
+        assertThrows(
+            Exception.class,
+            () -> {
+                transactionController.addTransaction(
+                    -1,
+                    wageTransaction,
+                    titleTranction,
+                    categoryTransaction,
+                    dateTransaction
+                );
+            }
+        );
     }
 }
