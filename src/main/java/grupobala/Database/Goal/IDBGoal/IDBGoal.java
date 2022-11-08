9092c0eff8dd1472b0449dd4942eef65619134ff
Felipe Rodrigues de Santana Freitag <felipefreitag08@hotmail.com>
2022-11-07 21:09:29 -0300

    feat: add get all method implementation in the DBGoal

--- src/main/java/grupobala/Database/Goal/IDBGoal/IDBGoal.java
+++ src/main/java/grupobala/Database/Goal/IDBGoal/IDBGoal.java
@@ -2,6 +2,7 @@ package grupobala.Database.Goal.IDBGoal;
 
 import grupobala.Entities.Goal.IGoal.IGoal;
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.Calendar;
 
 public interface IDBGoal {
@@ -16,4 +17,6 @@ public interface IDBGoal {
     public int removeGoal(int userID, int goalId) throws SQLException;
 
     public int updateGoal(int userID, IGoal goal) throws SQLException;
+
+    public ArrayList<IGoal> getAllGoals(int userID) throws SQLException;
 }
