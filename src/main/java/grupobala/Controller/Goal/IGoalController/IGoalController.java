2275617e5e29b114671360486224fcb4fd79a3ba
Henok Teixeira <henokteixeira@gmail.com>
2022-11-07 22:41:18 -0300

    feat: add goal controller interface and concrete class

---
+++ src/main/java/grupobala/Controller/Goal/IGoalController/IGoalController.java
@@ -0,0 +1,7 @@
+package grupobala.Controller.Goal.IGoalController;
+
+import java.util.Calendar;
+
+public interface IGoalController {
+    public void addGoal(int userID, String title, double objective, Calendar expectedDate, double idealValuePerMonth) throws Exception;
+}
