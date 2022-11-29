689843fa9b6d4bd648f31744ab65e56a1ed54c3e
Antônio Hugo <hugorplobo@gmail.com>
2022-11-29 20:22:58 -0300

    feat: add filter lambda interface

---
+++ src/main/java/grupobala/View/Components/Popups/FilterLambda.java
@@ -0,0 +1,7 @@
+package grupobala.View.Components.Popups;
+
+import grupobala.Entities.Extract.Filter.IFilter.IFilter;
+
+public interface FilterLambda {
+    public void applyOperation(IFilter filter);
+}
