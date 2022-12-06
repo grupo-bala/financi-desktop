1cb5250e569e76163deedc6f2d8f577578dff02b
Felipe Rodrigues de Santana Freitag <felipefreitag08@hotmail.com>
2022-12-06 17:09:47 -0300

    test: add tests for the database lesson class

---
+++ src/test/java/grupobala/Database/TestDBLesson.java
@@ -0,0 +1,62 @@
+package grupobala.Database;
+
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.Locale;
+
+import org.junit.jupiter.api.Test;
+
+import grupobala.Database.Connection.DBConnection;
+import grupobala.Database.Lesson.DBLesson;
+import grupobala.Database.Lesson.IDBLesson.IDBLesson;
+import grupobala.Entities.Lesson.ILesson.ILesson;
+import grupobala.SetupForTest.SetupForTest;
+
+public class TestDBLesson {
+
+    private IDBLesson databaseLesson = new DBLesson(
+        new DBConnection()
+    );
+
+    @Test
+    public void testGetAll() throws SQLException {
+        SetupForTest.truncateTables();
+
+        ArrayList<ILesson> lessons = this.databaseLesson.getAll(1);
+
+        assertNotEquals(0, lessons.size());
+    }
+
+    @Test
+    public void testUpdateWatchedAdd() throws SQLException {
+        SetupForTest.truncateTables();
+        int userId = SetupForTest.addFinanciUser();
+
+
+        assertDoesNotThrow(() -> {
+            this.databaseLesson.updateWatched(true, 1, userId);
+        });
+    }
+
+    @Test
+    public void testUpdateWatchedRemove() throws SQLException {
+        SetupForTest.truncateTables();
+        int userId = SetupForTest.addFinanciUser();
+
+        String query = String.format(
+            Locale.US,
+            "INSERT INTO aulaassistida(idusuario, idaula) VALUES (%d, %d)",
+            userId,
+            1
+        );
+
+        new DBConnection().executeUpdate(query);
+
+        assertDoesNotThrow(() -> {
+            this.databaseLesson.updateWatched(false, 1, userId);
+        });
+    }
+}
