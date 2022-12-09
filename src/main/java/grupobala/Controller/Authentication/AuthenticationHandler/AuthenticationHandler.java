012db6c4a2ea9a832b3b5044fbdffe898a1acfd7
Antônio Hugo <hugorplobo@gmail.com>
2022-12-09 13:57:39 -0300

    feat: implement chain of responsibility on signup

---
+++ src/main/java/grupobala/Controller/Authentication/AuthenticationHandler/AuthenticationHandler.java
@@ -0,0 +1,6 @@
+package grupobala.Controller.Authentication.AuthenticationHandler;
+
+public interface AuthenticationHandler {
+    public void setNext(AuthenticationHandler handler);
+    public void handle(String username, String password, String name, float wage) throws Exception;
+}
