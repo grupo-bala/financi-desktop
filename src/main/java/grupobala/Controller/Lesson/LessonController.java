5f6eb069b1372c1b2e2f7f6c272a0b75341b72f6
João Paulo Freitas Queiroz <Joaop3595@gmail.com>
2022-12-09 03:00:20 -0300

    feat: add lesson controller

---
+++ src/main/java/grupobala/Controller/Lesson/LessonController.java
@@ -0,0 +1,65 @@
+package grupobala.Controller.Lesson;
+
+import java.util.ArrayList;
+
+import grupobala.Controller.Lesson.ILessonController.ILessonController;
+import grupobala.Database.Connection.IDBConnection.IDBConnection;
+import grupobala.Database.Lesson.DBLesson;
+import grupobala.Database.Connection.DBConnection;
+import grupobala.Database.Course.DBCourse;
+import grupobala.Entities.Course.ICourse.ICourse;
+import grupobala.Entities.Lesson.ILesson.ILesson;
+import java.sql.SQLException;
+
+
+public class LessonController implements ILessonController{
+    IDBConnection databaseConnection = new DBConnection();
+    DBLesson dbLesson = new DBLesson(databaseConnection);
+    DBCourse dbCourse = new DBCourse(databaseConnection);
+
+    @Override
+    public ArrayList<ICourse> getCourses() throws SQLException, Exception {
+        try{
+            ArrayList<ICourse> cursos = dbCourse.getAll();
+            return cursos;
+        }
+        catch(SQLException error){
+            throw new Exception("Não foi possivel pegar os cursos");
+        }
+    }
+
+    @Override
+    public ArrayList<ILesson> getLessons(int courseId) throws SQLException, Exception{
+        
+        try{
+            ArrayList<ILesson> lessons =  dbLesson.getAll(courseId);
+            return lessons;
+        }
+        catch(SQLException error){
+            throw new Exception("Não foi possivel pegar as aulas");
+        }     
+    }
+
+    @Override
+    public void updateWatched(boolean status, int lessonId, int userId) throws SQLException, Exception {
+        try{
+            dbLesson.updateWatched(status, lessonId, userId);
+        }
+        catch(SQLException error){
+            throw new Exception("Não foi possivel atualizar o status da aula");
+        }     
+    }
+
+    public ILesson getLesson(int courseId, int lessonId) throws SQLException, Exception {
+        ArrayList<ILesson> lessons =  getLessons(courseId);
+        for(ILesson lesson : lessons){
+            if(lessonId == lesson.getId()){
+                return lesson;
+            }
+        }
+        throw new Exception("Aula não existe");
+    }
+
+
+    
+}
