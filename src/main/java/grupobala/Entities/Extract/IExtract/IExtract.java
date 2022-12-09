fa8f098398bf2a2d851b8a8445d87c2a1019fd3e
MLeandr0 <matheusleandro33@gmail.com>
2022-12-09 13:05:23 -0300

    feat: implementing iterator

--- src/main/java/grupobala/Entities/Extract/IExtract/IExtract.java
+++ src/main/java/grupobala/Entities/Extract/IExtract/IExtract.java
@@ -1,9 +1,11 @@
 package grupobala.Entities.Extract.IExtract;
 
+import grupobala.Entities.Iterator.IterableInterface;
 import grupobala.Entities.Transaction.ITransaction.ITransaction;
 
-public interface IExtract extends Iterable<ITransaction> {
-    public double getEntry();
+public interface IExtract extends IterableInterface <ITransaction> {
+
+    public double  getEntry();
 
     public double getOutput();
 }
